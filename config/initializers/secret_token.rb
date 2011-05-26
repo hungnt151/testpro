3f322b58b35f8d8f2e1923a5f91cddebc95c2179
HungNT <hungnt@hungnt-laptop.(none)>
2011-05-26 08:39:30 +0700

    Empty projec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Polymorphic::Application.config.secret_token = '2a3f8cca82a816de5dad4aec0adedf0a590315fb458f2af01219f1abc22954e2f6c5b10764730b88094635221ef11136ffb273e9afb26a208dfb854bd9cae378'
